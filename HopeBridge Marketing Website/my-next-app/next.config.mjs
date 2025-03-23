a7b619f24dd75d5554c3b3afbd693b227cbd8c17
gihan-r <kavimcg03@gmail.com>
2025-03-23 17:32:11 +0530

    Update Next.js config to fix image loading

--- HopeBridge Marketing Website/my-next-app/next.config.mjs
+++ HopeBridge Marketing Website/my-next-app/next.config.mjs
@@ -3,10 +3,23 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   basePath: '/HopeBridge-',
   assetPrefix: '/HopeBridge-/',
   trailingSlash: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
